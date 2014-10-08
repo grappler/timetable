595c92aa598d0393582182db0179ce20b2cf80f9
Ulrich Pogson <grapplerulrich@gmail.com>
2014-10-08 22:03:59 +0200

    Control number of days displayed

--- public/partials/column-header.php
+++ public/partials/column-header.php
@@ -1,8 +1,10 @@
 <?php
+$data = timetable_get_static_data();
 $week = timetable_get_week();
+$number_days = count( $data['days'] );
 
 foreach( $week as $day ) { ?>
-	<div title="<?php echo $day['weekday_whole']; ?>" class="column ">
+	<div title="<?php echo $day['weekday_whole']; ?>" class="column days-<?php echo $number_days; ?>">
 		<span data-full-date="<?php echo $day['weekday_abbrev']; ?>"><?php echo $day['weekday_abbrev']; ?></span>
 	</div><!-- header column -->
 <?php }
--- public/partials/columns.php
+++ public/partials/columns.php
@@ -2,12 +2,12 @@
 $data = timetable_get_static_data();
 $grouped_posts = timetable_get_grouped_sessions();
 $number_minutes = $data['time']['minutes'];
-$number_days = '6';
+$number_days = count( $data['days'] );
 
 // split them into 7 arrays
 foreach ( $grouped_posts as $day => $day_posts ) {
 ?>
-	<div class="timetable-mobile-day column timetable-column <?php echo $number_minutes; ?>-minutes <?php echo $number_minutes; ?>-days" style="height:<?php echo $number_minutes; ?>px;">
+	<div class="timetable-mobile-day column timetable-column minutes-<?php echo $number_minutes; ?> days-<?php echo $number_days; ?>">
 		<?php foreach( $grouped_posts[ $day ] as $day_post ) {
 			print_session_template( $day_post );
 		} ?>
